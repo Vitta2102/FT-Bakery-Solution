e0cffdf7112238fe602a9635fb291320b603a8f1
Vitta2102 <e260781@dac.unicamp.br>
2023-06-03 22:05:23 -0300

    Adicionar arquivos de projeto.

---
+++ FT Bakery/FT Bakery.cpp
@@ -0,0 +1,12 @@
+﻿// FT Bakery.cpp: define o ponto de entrada para o aplicativo.
+//
+
+#include "FT Bakery.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ FT Bakery/a02ex03.hpp
@@ -0,0 +1,31 @@
+/* Unicamp - Universidade Estadual de Campinas
+   FT - Faculdade de Tecnologia
+   Limeira - SP
+   Prof. Dr. Andre F. de Angelis
+   Mar/2016
+*/
+
+using namespace std;
+
+#ifndef A02EX03_H
+   #define A02EX03_H
+
+   void process(void);
+   void verifyArguments(int, char* []);
+   void clearAll(void);
+   
+   void listItems(void);
+   void insertItems(void);
+   void insertBread(void);
+   void insertCheese(void);
+   void insertCottageCheese(void);
+   void insertHam(void);
+   void insertMortadella(void);
+   void insertCracker(void);
+   void insertFilledWafer(void);
+   
+   
+   
+#endif
+
+/* fim de arquivo */
